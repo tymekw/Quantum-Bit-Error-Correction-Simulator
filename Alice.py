340711b7bb0202c298f2a575491e8fda8a97acb4
tymek <tymwid22@gmail.com>
2021-04-21 16:27:20 +0200

    added tpm class and alice

---
+++ Alice.py
@@ -0,0 +1,39 @@
+import TPM
+import numpy as np
+N = 8
+K = 6
+L = 2
+
+W = np.random.randint(-L, L+1, size=(K,N))
+W_bob = np.random.randint(-L, L+1, size=(K,N))
+
+alice = TPM.Tpm(N, K, L, W)
+bob = TPM.Tpm(N, K, L, W_bob)
+
+X = np.random.randint(-L, L + 1, size=(K, N))
+alice.calculate_tau(X)
+bob.calculate_tau(X)
+
+while alice.tau != bob.tau:
+    X = np.random.randint(-L, L + 1, size=(K, N))
+    X_bob = np.random.randint(-L, L + 1, size=(K, N))
+    alice.calculate_tau(X)
+    bob.calculate_tau(X)
+    print('s')
+
+s= 0
+while not np.array_equal(alice.W, bob.W):
+    alice.update_weights(X)
+    bob.update_weights(X)
+
+    X = np.random.randint(-L, L + 1, size=(K, N))
+    alice.calculate_tau(X)
+    bob.calculate_tau(X)
+    while alice.tau != bob.tau:
+        X = np.random.randint(-L, L + 1, size=(K, N))
+        alice.calculate_tau(X)
+        bob.calculate_tau(X)
+
+
+    s+=1
+    print(s)
---
+++ TPM.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+
+def calculate_theta(sigma_k, tau):
+    return 0 if sigma_k != tau else 1
+
+
+class Tpm:
+    def __init__(self, n, k, l, w):
+        self.sigma = None
+        self.tau = None
+        self.N = n
+        self.K = k
+        self.L = l
+        self.W = w
+
+    def calculate_tau(self, X):
+        sigma = np.sign(np.sum(X * self.W, axis=1))
+        sigma = np.array([-1 if x == 0 else x for x in sigma])
+        tau = np.prod(sigma)
+        self.tau = tau
+        self.sigma = sigma
+        # return tau
+
+    def update_weights(self, X):
+        self.calculate_tau(X)
+        w_new = np.empty([self.K, self.N])
+        for (k, n), _ in np.ndenumerate(X):
+            z = self.W[k, n] + X[k, n] * self.sigma[k] * calculate_theta(self.sigma[k], self.tau)
+            if z <= -self.L:
+                w_new[k, n] = int(-self.L)
+            elif z >= self.L:
+                w_new[k, n] = int(self.L)
+            else:
+                w_new[k, n] = int(z)
+
+        self.W = w_new
+        return w_new
