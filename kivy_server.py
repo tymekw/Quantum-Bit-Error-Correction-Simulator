f52799ee08e616e20f0c1acba5ead6080b44a217
tymekw <tymwid22@gmail.com>
2021-07-16 13:22:05 +0200

    delete popup classes

--- kivy_server.py
+++ kivy_server.py
@@ -2,22 +2,11 @@ from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.popup import Popup
 from kivy.uix.slider import Slider
-import threading
-
 from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
 import alice_server
-
-
-class WaitingForClientToBindPopup(GridLayout):
-    label = Label(text="whatever")
-
-class WaitingForClientToReceivePopup(GridLayout):
-    label = Label(text="whatever")
-
-class WaitingForRunPopup(GridLayout):
-    label = Label(text="waiting for run")
+import threading
 
 
 class ServerLayout(GridLayout):
@@ -35,7 +24,7 @@ class ServerLayout(GridLayout):
 
         self.bits_label = Label(text="bits length")
         self.seed_label = Label(text="write seed")
-        self.bits_value = Label(text="100")
+        self.bits_value = Label(text="1000")
 
         self.machine_details_layout.add_widget(self.n_label)
         self.machine_details_layout.add_widget(self.n_value)
@@ -51,13 +40,13 @@ class ServerLayout(GridLayout):
         self.add_widget(self.machine_details_layout)
 
         self.machine_sliders_text_layout = GridLayout(cols=1)
-        self.n_slider = Slider(min=0, max=100, value=50)
+        self.n_slider = Slider(min=1, max=100, value=10)
         self.n_slider.bind(value=self.on_slider_n)
-        self.k_slider = Slider(min=0, max=100, value=50)
+        self.k_slider = Slider(min=1, max=100, value=10)
         self.k_slider.bind(value=self.on_slider_k)
-        self.l_slider = Slider(min=0, max=100, value=50)
+        self.l_slider = Slider(min=1, max=100, value=10)
         self.l_slider.bind(value=self.on_slider_l)
-        self.bits_slider = Slider(min=0, max=10000, value=100)
+        self.bits_slider = Slider(min=1, max=10000, value=1000)
         self.bits_slider.bind(value=self.on_slider_bits)
         self.seed_text_field = TextInput(text='Write your seed')
 
@@ -74,8 +63,10 @@ class ServerLayout(GridLayout):
         self.bind_button.bind(on_press=self.on_bind)
         self.create_bits_button = Button(text="Create random bits")
         self.create_bits_button.bind(on_press=self.on_create_bits)
-        self.import_bits_button = Button(text="Import bits")
+
+        self.import_bits_button = Button(text="Import bits", disabled=True)
         # self.import_bits_button.bind(on_press=self.function)
+
         self.send_machine_config_button = Button(text="Send machine config")
         self.send_machine_config_button.bind(on_press=self.on_send_config)
         self.run_machine_button = Button(text="Run machine")
@@ -90,7 +81,7 @@ class ServerLayout(GridLayout):
         self.add_widget(self.buttons_layout)
 
         self.bits_layout = GridLayout(cols=1)
-        self.bits_label_all = TextInput(text='111', disabled=True)
+        self.bits_label_all = TextInput(text='111', disabled=True, cursor=(0,0))
         self.bits_layout.add_widget(self.bits_label_all)
         self.add_widget(self.bits_layout)
 
@@ -127,8 +118,7 @@ class ServerLayout(GridLayout):
                 return True
 
     def show_bind_popup(self):
-        popup = WaitingForClientToBindPopup()
-        self.popup_window = Popup(title="waiting", content=popup, size_hint=(None, None), size=(400, 400),
+        self.popup_window = Popup(title="waiting", size_hint=(None, None), size=(400, 400),
                                   auto_dismiss=False)
         self.popup_window.open()
 
@@ -160,19 +150,11 @@ class ServerLayout(GridLayout):
                 return True
 
     def show_send_popup(self):
-        popup = WaitingForClientToReceivePopup()
-        self.popup_window = Popup(title="waiting", content=popup, size_hint=(None, None), size=(400, 400),
+        self.popup_window = Popup(title="waiting", size_hint=(None, None), size=(400, 400),
                                   auto_dismiss=False)
         self.popup_window.open()
 
     def on_run_machine(self, instance):
-        # self.n_slider.disabled = True
-        # self.k_slider.disabled = True
-        # self.l_slider.disabled = True
-        # self.bits_slider.disabled = True
-        # self.seed_text_field.disabled = True
-        # self.create_bits_button.disabled = True
-        # instance.disabled = True
         self.alice.create_machine()
         self.show_run_popup()
         self.run_thread = threading.Thread(target=self.alice.run_machine)
@@ -186,18 +168,21 @@ class ServerLayout(GridLayout):
         while True:
             if not self.run_thread.is_alive():
                 self.popup_window.dismiss()
-                print(self.alice.bits.bits)
-                print(self.alice.W)
-                print(self.alice.bits.max_val)
-                print(len(self.alice.bits.bits))
                 self.bits_label_all.text = str(self.alice.bits.bits)
                 self.bits_label_all.disabled = False
-
+                self.n_slider.disabled = False
+                self.k_slider.disabled = False
+                self.l_slider.disabled = False
+                self.bits_slider.disabled = False
+                self.seed_text_field.disabled = False
+                self.create_bits_button.disabled = False
+                self.bind_button.disabled = False
+                self.run_machine_button.disabled = False
+                self.send_machine_config_button.disabled = False
                 return True
 
     def show_run_popup(self):
-        popup = WaitingForRunPopup()
-        self.popup_window = Popup(title="running machine", content=popup, size_hint=(None, None), size=(400, 400),
+        self.popup_window = Popup(title="running machine", size_hint=(None, None), size=(400, 400),
                                   auto_dismiss=False)
         self.popup_window.open()
 
