ab53ca461a559ec6b7a1d85725b845c8e8edea51
tymekw <tymwid22@gmail.com>
2021-10-10 11:02:39 +0200

    move testing scripts

---
+++ test/alice_server.py
@@ -0,0 +1,149 @@
+import math
+import socket
+import pickle
+import threading
+
+import bits
+import numpy as np
+import TPM
+
+
+class AliceServer:
+    def __init__(self):
+        self.N = 10
+        self.K = 5
+        self.L = 2
+        self.HOST = '127.0.0.1'
+        self.PORT = 65432
+        self.s = None
+        self.conn = None
+        self.addr = None
+        self.machine_conf = None
+        self.W = None
+        self.alice = None
+        self.X = None
+        self.bits = bits.Bits(self.L)
+        self.seed = 1
+        self.bits_length = 1000
+
+    def set_bits_length(self, length):
+        self.bits_length = length
+
+    def set_seed(self, seed):
+        self.seed = seed
+
+    def set_N(self, N):
+        self.N = N
+
+    def set_K(self, K):
+        self.K = K
+
+    def set_L(self, L):
+        self.L = L
+
+    def choose_machine_details(self, N, K, L):
+        self.N = N
+        self.K = K
+        self.L = L
+
+    def bind(self):
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        print("before bind")
+        self.s.bind((self.HOST, self.PORT))
+        print("bind")
+
+        self.s.listen()
+        self.conn, self.addr = self.s.accept()
+        print("binded")
+        return True
+
+    def generate_bits(self):
+        self.bits.generate_bits(self.seed, self.bits_length)
+
+    def get_possible_N_K(self, w_len):
+        factors = self.get_factors_list(w_len)
+        return self.get_best_pair(factors)
+        # return factors
+
+    def get_factors_list(self, w_len):
+        factors = []
+        for i in range(1, w_len + 1):
+            if w_len % i == 0:
+                factors.append((i, int(w_len / i)))
+        return factors
+
+    def get_best_pair(self, factors):
+        best_pair = None
+        tmp = math.inf
+        for factor_pair in factors:
+            dif = max(factor_pair) - min(factor_pair)
+            if tmp > dif:
+                best_pair = factor_pair
+                tmp = dif
+        return best_pair
+
+    # def check_N_K(self, N, K, bits_len):
+    #     return N * K < bits_len
+
+    def create_machine(self):
+        W_len = len(self.bits.bits_to_w())
+        self.N, self.K = self.get_possible_N_K(W_len)
+        self.W = self.bits.bits_to_arr(self.K, self.N)
+        self.alice = TPM.Tpm(self.N, self.K, self.L, self.W)
+
+    def send_machine_config(self):
+        self.machine_conf = [self.N, self.K, self.L, self.seed, self.bits_length]
+        data = pickle.dumps(self.machine_conf)
+        self.conn.sendall(data)
+        rec = self.conn.recv(1024)
+        if pickle.loads(rec) == 'OK':
+            return True
+
+    def run_machine(self):
+        print("waits")
+        print(self.W)
+        print(self.alice.W)
+        for i in range(0, 150):
+            print("inside loop")
+            bob_tau = None
+            self.alice.tau = 1
+            while self.alice.tau != bob_tau:
+                print("choose X")
+                try:
+                    self.s.listen()
+                    self.conn, self.addr = self.s.accept()
+                except:
+                    print("socket error")
+
+                self.X = np.random.randint(-self.L, self.L + 1, size=(self.K, self.N))
+                self.alice.calculate_tau(self.X)
+                print("sending X")
+                data = pickle.dumps(self.X)
+                self.conn.sendall(data)
+                print("X send")
+                rec_tau = self.conn.recv(1000000)
+                bob_tau = pickle.loads(rec_tau)
+
+                if self.alice.tau == bob_tau:
+                    data = pickle.dumps(1)
+                    self.conn.sendall(data)
+                    print("X chosen")
+                else:
+                    data = pickle.dumps(0)
+                    self.conn.sendall(data)
+                    print("X not chosen")
+            print(i)
+            self.alice.update_weights(self.X)
+
+        print("outside loop")
+
+        #ToDo check if W are the same
+        bob_w = self.conn.recv(1000000)
+        bob_wei = pickle.loads(bob_w)
+        if np.array_equal(self.alice.W, bob_wei):
+            print('dziala')
+
+        print("done")
+
+        print(self.alice.W)
+        self.bits.bits = self.bits.arr_to_bits(self.alice.W, self.bits_length)
---
+++ test/bits.py
@@ -0,0 +1,87 @@
+import random
+import numpy as np
+import math
+
+class Bits:
+    def __init__(self, L):
+        self.bits = None
+        self.BER = 3
+        self.type = 'random'
+        self.L = L
+        self.max_val = 2*self.L
+
+    def change_L(self, L):
+        self.L = L
+        self.max_val = 2 * self.L + 1 + self.L // 2
+
+    def generate_bits(self, seed, length):
+        random.seed(seed)
+        b = random.getrandbits(length)
+        b = b
+        b = bin(b)
+        b = b[2:]
+        if len(b) != length:
+            b = '0' * (length - len(b)) + b
+        self.bits = b
+
+    def create_BER(self):
+        b_list = [i for i in self.bits]
+        if self.type == 'random':
+            for _ in range(int(len(b_list) * 0.01*self.BER)):
+                if b_list[random.randint(0, len(b_list) - 1)] == "1":
+                    b_list[random.randint(0, len(b_list) - 1)] = "0"
+                else:
+                    b_list[random.randint(0, len(b_list) - 1)] = "1"
+
+        elif self.type == 'block':
+            how_many_to_change = int(len(b_list) * 0.01*self.BER)
+            start_idx = random.randint(0, len(b_list)- 1 - how_many_to_change)
+            for i in range(how_many_to_change):
+                if b_list[start_idx+i] == "1":
+                    b_list[start_idx + i] = "0"
+                else:
+                    b_list[start_idx + i] = "1"
+
+        self.bits = "".join(b_list)
+    def bits_to_w(self):
+        min_req_bits = math.ceil(math.log2(self.max_val + 1))
+        bits_list = [self.bits[i:i + min_req_bits] for i in range(0, len(self.bits), min_req_bits)]
+        if len(bits_list[-1]) < min_req_bits:
+            bits_list[-1] = '0' * (min_req_bits - len(bits_list[-1])) + bits_list[-1]
+        nums = [int("0b" + str(bit), 2) - self.max_val // 2 for bit in bits_list]
+        self.first_bin = None
+        self.next_new = None
+        nums = self.handle_bits_outside_range(nums)
+        arr = np.array(nums)
+        return arr
+
+    def bits_to_arr(self, K, N):
+        return np.reshape(self.bits_to_w(), (K, N))
+
+    def handle_bits_outside_range(self, nums):
+        for i, element in enumerate(nums):
+            if element > self.L:
+                if not self.first_bin:
+                    self.first_bin = bin(element + self.max_val // 2)
+                    self.next_new = int('0b' + self.first_bin[3:], 2)
+                else:
+                    if self.next_new < self.max_val:
+                        self.next_new = self.next_new + 1
+                    else:
+                        self.next_new = 0
+                element = self.next_new - self.max_val // 2
+                nums[i] = element
+        return nums
+
+    def arr_to_bits(self, arr, length):
+        al = []
+        for row in arr:
+            for el in row:
+                al.append(el + self.max_val // 2)
+        result_bits = [bin(int(i))[2:] for i in al]
+        max_len = len(max(result_bits, key=len))
+        b = ['0' * (max_len - len(bit)) + bit for bit in result_bits]
+        b = ''.join(b)
+        return b[:length]
+
+
