b950539ad6572e53bcd45c7e812ae7d0e68d033c
tymekw <tymwid22@gmail.com>
2021-11-12 17:15:17 +0100

    clean up

--- bits.py
+++ bits.py
@@ -47,33 +47,20 @@ class Bits:
         self.bits = "".join(b_list)
 
     def bits_to_w(self):
-            min_req_bits = math.ceil(math.log2(self.L + 1)) + 1
-            bits_list = [self.bits[i:i + min_req_bits] for i in range(0, len(self.bits), min_req_bits)]
-            if len(bits_list[-1]) < min_req_bits:
-                bits_list[-1] = '0' * (min_req_bits - len(bits_list[-1])) + bits_list[-1]
-            # nums = [int("0b" + str(bit), 2) - self.max_val // 2 for bit in bits_list]
-            nums = []
-            for number in bits_list:
-                sign = 1 if number[0] == 0 else -1
-                nums.append(sign*int("0b" +str(number[1:]),2))
-            self.first_bin = None
-            self.next_new = None
-            nums = self.handle_bits_outside_range(nums)
-            arr = np.array(nums)
-            # print(nums)
-            return arr
+        min_req_bits = math.ceil(math.log2(self.L + 1)) + 1
+        bits_list = [self.bits[i:i + min_req_bits] for i in range(0, len(self.bits), min_req_bits)]
+        if len(bits_list[-1]) < min_req_bits:
+            bits_list[-1] = '0' * (min_req_bits - len(bits_list[-1])) + bits_list[-1]
+        nums = []
+        for number in bits_list:
+            sign = 1 if number[0] == 0 else -1
+            nums.append(sign*int("0b" +str(number[1:]),2))
+        self.first_bin = None
+        self.next_new = None
+        nums = self.handle_bits_outside_range(nums)
+        arr = np.array(nums)
+        return arr
 
-    # def bits_to_w(self):
-    #     min_req_bits = math.ceil(math.log2(self.max_val + 1))
-    #     bits_list = [self.bits[i:i + min_req_bits] for i in range(0, len(self.bits), min_req_bits)]
-    #     if len(bits_list[-1]) < min_req_bits:
-    #         bits_list[-1] = '0' * (min_req_bits - len(bits_list[-1])) + bits_list[-1]
-    #     nums = [int("0b" + str(bit), 2) - self.max_val // 2 for bit in bits_list]
-    #     self.first_bin = None
-    #     self.next_new = None
-    #     nums = self.handle_bits_outside_range(nums)
-    #     arr = np.array(nums)
-    #     return arr
 
     def bits_to_arr(self, K, N):
         return np.reshape(self.bits_to_w(), (K, N))
@@ -93,22 +80,9 @@ class Bits:
                 nums[i] = element
         return nums
 
-    # def arr_to_bits(self, arr, length):
-    #     al = []
-    #     for row in arr:
-    #         for el in row:
-    #             al.append(el + self.max_val // 2)
-    #     result_bits = [bin(int(i))[2:] for i in al]
-    #     max_len = len(max(result_bits, key=len))
-    #     b = ['0' * (max_len - len(bit)) + bit for bit in result_bits]
-    #     b = ''.join(b)
-    #     return b[:length]
+
     def arr_to_bits(self, arr, length):
-        number_list = [i for i in [j for j in arr]]
-        # al = []
-        # for row in arr:
-        #     for el in row:
-        #         al.append(el)
+        number_list = [i for sublists in arr.tolist() for i in sublists]
         result_bits  = ["1"+bin(int(i))[3:] if int(i)<0  else "0"+bin(int(i))[2:] for i in number_list]
         max_len = len(max(result_bits, key=len))
         b = ['0' * (max_len - len(bit)) + bit for bit in result_bits]
