e60544997706f98c9d37f37c658fd134959330ff
tymekw <tymwid22@gmail.com>
2023-06-17 11:00:17 +0200

    results and stats

--- data_processor/predict.py
+++ data_processor/predict.py
@@ -4,12 +4,13 @@ from typing import Tuple, Any
 import numpy as np
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.svm import SVR
 
 from neural_crypto.data_processor.common import (
     RANDOM_QBER_DATA_PATH,
     RANDOM_QBER_TEST_DATA_PATH,
     BURSTY_QBER_DATA_PATH,
-    BURSTY_QBER_TEST_DATA_PATH,
+    BURSTY_QBER_TEST_DATA_PATH, RANDOM_QBER_STATS_DATA_PATH,
 )
 
 
@@ -25,14 +26,32 @@ def get_all_observations_and_targets(filename: str) -> Tuple[np.array, np.array]
 
     return np.array(observations), np.array(targets)
 
+def get_max_observations_and_targets(stats_filename: str) -> Tuple[np.array, np.array]:
+    with open(stats_filename, 'r') as data_file:
+        reader = csv.reader(data_file)
+        next(reader, None) # skip header
+        observations = []
+        targets = []
+        for row in reader:
+            observations.append([int(i) for i in row[0:4]])
+            targets.append(int(row[4]))
+
+    return np.array(observations), np.array(targets)
+
+
+
+
+
 def predict(training_data: Tuple[np.array, np.array], testing_data: Tuple[np.array, np.array]) -> Any:
     X, Y = training_data
     X_test, Y_test = testing_data
     linear_regression = LinearRegression().fit(X, Y)
     Y_pred = linear_regression.predict(X_test)
+    # svr_poly = SVR(kernel="poly").fit(X, Y)
+    # Y_pred = svr_poly.predict(X_test)
     print(f'Mean squared error: {mean_squared_error(Y_test, Y_pred)}')
     print(f'Coefficient of determination: {r2_score(Y_test, Y_pred)}')
-    return linear_regression
+    return Y_pred
 
 if __name__ == '__main__':
     random_qber_training_data = get_all_observations_and_targets(RANDOM_QBER_DATA_PATH)
@@ -41,6 +60,16 @@ if __name__ == '__main__':
     bursty_qber_training_data = get_all_observations_and_targets(BURSTY_QBER_DATA_PATH)
     bursty_qber_testing_data = get_all_observations_and_targets(BURSTY_QBER_TEST_DATA_PATH)
 
-    random_qber_linear_regression = predict(random_qber_training_data, random_qber_testing_data)
-    bursty_qber_linear_regression = predict(bursty_qber_training_data, bursty_qber_testing_data)
+    random_qber_predicted = predict(random_qber_training_data, random_qber_testing_data)
+    bursty_qber_predicted = predict(bursty_qber_training_data, bursty_qber_testing_data)
 
+
+    random_qber_training_data_max = get_max_observations_and_targets(RANDOM_QBER_STATS_DATA_PATH)
+    random_qber_testing_data = get_all_observations_and_targets(RANDOM_QBER_TEST_DATA_PATH)
+
+    # bursty_qber_training_data = get_all_observations_and_targets(BURSTY_QBER_DATA_PATH)
+    # bursty_qber_testing_data = get_all_observations_and_targets(BURSTY_QBER_TEST_DATA_PATH)
+
+    # random_qber_on_max_predicted = predict(random_qber_training_data_max, random_qber_testing_data)
+    # print('OK')
+    # bursty_qber_predicted = predict(bursty_qber_training_data, bursty_qber_testing_data)
--- data_processor/prepare.py
+++ data_processor/prepare.py
@@ -1,5 +1,6 @@
 import csv
 
+import numpy as np
 
 from neural_crypto.data_processor.common import (
     RANDOM_QBER_TEST_DATA_PATH,
@@ -10,6 +11,11 @@ from neural_crypto.data_processor.common import (
     QBERType,
     RAW_TESTING_DATASET_PATH,
     RAW_TRAINING_DATASET_PATH,
+    STATS_DATA_HEADER,
+    RANDOM_QBER_TEST_STATS_DATA_PATH,
+    BURSTY_QBER_TEST_STATS_DATA_PATH,
+    RANDOM_QBER_STATS_DATA_PATH,
+    BURSTY_QBER_STATS_DATA_PATH,
 )
 
 def write_prepared_data(path: str, is_test_data: bool=False) -> None:
@@ -32,8 +38,34 @@ def write_prepared_data(path: str, is_test_data: bool=False) -> None:
                         elif row[5] == QBERType.BURSTY:
                             bursty_qber_writer.writerow(required_data)
 
+def write_prepared_data_with_statistics(path: str, path_to_write: str) -> None:
+    with open(path, 'r') as file:
+        reader = csv.reader(file,  delimiter =';')
+
+        is_next = next(reader, None)
+        with open(path_to_write, 'w+', newline='') as stats_file:
+            stats_writer = csv.writer(stats_file)
+            stats_writer.writerow(STATS_DATA_HEADER)
+            while is_next:
+                rows = [next(reader, None) for _ in range(6)]
+                is_next = None not in rows
+                if is_next:
+                    REPS = [int(row[0].split(',')[-1]) for row in rows]
+                    max_reps = str(max(REPS))
+                    min_reps = str(min(REPS))
+                    mean = str(int(np.mean(REPS)))
+                    median = str(int(np.median(REPS)))
+                    std_dev = str(np.std(REPS))
+                    variance = str(np.var(REPS))
+                    row_with_stats = rows[0][0].split(',')[0:4] + [max_reps, min_reps, mean, median, std_dev, variance]
+                    stats_writer.writerow(row_with_stats)
+
 
 
 if __name__ == '__main__':
     write_prepared_data(RAW_TRAINING_DATASET_PATH, is_test_data=False)
     write_prepared_data(RAW_TESTING_DATASET_PATH, is_test_data=True)
+    write_prepared_data_with_statistics(RANDOM_QBER_DATA_PATH, RANDOM_QBER_STATS_DATA_PATH)
+    write_prepared_data_with_statistics(BURSTY_QBER_DATA_PATH, BURSTY_QBER_STATS_DATA_PATH)
+    write_prepared_data_with_statistics(RANDOM_QBER_TEST_DATA_PATH, RANDOM_QBER_TEST_STATS_DATA_PATH)
+    write_prepared_data_with_statistics(BURSTY_QBER_TEST_DATA_PATH, BURSTY_QBER_TEST_STATS_DATA_PATH)
