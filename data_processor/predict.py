e3225ad3cffc24c43ea0be20ff8e131c7f290373
tymekw <tymwid22@gmail.com>
2023-06-19 07:24:06 +0200

    sim and test predict

--- data_processor/predict.py
+++ data_processor/predict.py
@@ -1,6 +1,7 @@
 import csv
 from typing import Tuple, Any
 
+import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
@@ -16,7 +17,7 @@ from neural_crypto.data_processor.common import (
 
 def get_all_observations_and_targets(filename: str) -> Tuple[np.array, np.array]:
     with open(filename, 'r') as data_file:
-        reader = csv.reader(data_file)
+        reader = csv.reader(data_file, delimiter=';')
         next(reader, None) # skip header
         observations = []
         targets = []
@@ -28,7 +29,7 @@ def get_all_observations_and_targets(filename: str) -> Tuple[np.array, np.array]
 
 def get_max_observations_and_targets(stats_filename: str) -> Tuple[np.array, np.array]:
     with open(stats_filename, 'r') as data_file:
-        reader = csv.reader(data_file)
+        reader = csv.reader(data_file, delimiter=';')
         next(reader, None) # skip header
         observations = []
         targets = []
@@ -53,19 +54,50 @@ def predict(training_data: Tuple[np.array, np.array], testing_data: Tuple[np.arr
     print(f'Coefficient of determination: {r2_score(Y_test, Y_pred)}')
     return Y_pred
 
+def display_data(observations: np.array, targets: np.array, predicted: np.array) -> None:
+    with open(RANDOM_QBER_DATA_PATH, 'r') as f:
+        temp = f.read().splitlines()
+        lines = [line.split(';') for line in temp][1:]
+        data = [[float(i) for i in row] for row in lines]
+        # L, N*K, QBER, MAX, MIN, MEAN, MEDIAN, STD_DEV, VAR
+        data = [[row[0], row[1] * row[2], *row[3:]] for row in data]
+        data.sort(key=lambda x: (x[0], x[2], x[1])) # sorted by L, QBER than N*K
+        l_3 = list(filter(lambda x: x[0] == 3.0, data)) # only with L==3
+        n_ks = [row[1] for row in l_3]
+        y = [row[3] for row in l_3]
+        plt.scatter(n_ks, y)
+        plt.show()
+
+
+        qber_8 = list(filter(lambda x: x[2] == 8.0, l_3)) # only with QBER==8
+        n_ks = [row[1] for row in qber_8]
+        y = [row[5] for row in qber_8]
+        plt.scatter(n_ks, y)
+        plt.show()
+
+    print('OK')
+
+
+
+
+
+
+
 if __name__ == '__main__':
     random_qber_training_data = get_all_observations_and_targets(RANDOM_QBER_DATA_PATH)
     random_qber_testing_data = get_all_observations_and_targets(RANDOM_QBER_TEST_DATA_PATH)
 
-    bursty_qber_training_data = get_all_observations_and_targets(BURSTY_QBER_DATA_PATH)
-    bursty_qber_testing_data = get_all_observations_and_targets(BURSTY_QBER_TEST_DATA_PATH)
+    # bursty_qber_training_data = get_all_observations_and_targets(BURSTY_QBER_DATA_PATH)
+    # bursty_qber_testing_data = get_all_observations_and_targets(BURSTY_QBER_TEST_DATA_PATH)
 
     random_qber_predicted = predict(random_qber_training_data, random_qber_testing_data)
-    bursty_qber_predicted = predict(bursty_qber_training_data, bursty_qber_testing_data)
+    # bursty_qber_predicted = predict(bursty_qber_training_data, bursty_qber_testing_data)
+    #
+    #
+    # random_qber_training_data_max = get_max_observations_and_targets(RANDOM_QBER_STATS_DATA_PATH)
+    # random_qber_testing_data = get_all_observations_and_targets(RANDOM_QBER_TEST_DATA_PATH)
 
-
-    random_qber_training_data_max = get_max_observations_and_targets(RANDOM_QBER_STATS_DATA_PATH)
-    random_qber_testing_data = get_all_observations_and_targets(RANDOM_QBER_TEST_DATA_PATH)
+    display_data(*random_qber_training_data, random_qber_predicted)
 
     # bursty_qber_training_data = get_all_observations_and_targets(BURSTY_QBER_DATA_PATH)
     # bursty_qber_testing_data = get_all_observations_and_targets(BURSTY_QBER_TEST_DATA_PATH)
@@ -73,3 +105,10 @@ if __name__ == '__main__':
     # random_qber_on_max_predicted = predict(random_qber_training_data_max, random_qber_testing_data)
     # print('OK')
     # bursty_qber_predicted = predict(bursty_qber_training_data, bursty_qber_testing_data)
+
+
+
+
+
+
+
